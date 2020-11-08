9b3b7e1af22c2f6ba1084858a899af84163cbc2a
Gabis1810 <74035563+Gabis1810@users.noreply.github.com>
2020-11-07 23:53:25 -0600

    Add files via upload

---
+++ Week 3 - Quiz 2.R
@@ -0,0 +1,34 @@
+# Carlos Gabriel Guerra Farfán
+# R Programming
+# Week 3
+# Quiz 2
+
+# Question 1
+# makeCacheMatrix: This function creates a special "matrix" object that can cache its inverse
+
+makeCacheMatrix <- function(x = matrix()) {
+  inverse <- NULL
+  set <- function(y){
+    x <<- y
+    inverse <<- NULL
+  }
+  get <- function() x
+  setinverse <- function(solveMatrix) inverse <<- solveMatrix
+  getinverse <- function() inverse
+  list(set = set, get = get, setinverse = setinverse, getinverse = getinverse)
+}
+
+# Question 2
+# cacheSolve: This function computes the inverse of the special "matrix" returned by makeCacheMatrix above
+
+cacheSolve <- function(x, ...) {
+  inverse <- x$getinverse()
+  if(!is.null(inverse)){
+    message("Cached Data")
+    return(inverse)
+  }
+  data <- x$get()
+  inverse <- solve(data)
+  x$setinverse(inverse)
+  inverse      
+}
