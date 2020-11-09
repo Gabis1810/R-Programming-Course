31082015c613112ad5af87f727952cbaf0257b70
Gabis1810 <74035563+Gabis1810@users.noreply.github.com>
2020-11-09 16:46:24 -0600

    Add files via upload

--- cachematrix.R
+++ cachematrix.R
@@ -7,14 +7,26 @@
 # makeCacheMatrix: This function creates a special "matrix" object that can cache its inverse
 
 makeCacheMatrix <- function(x = matrix()) {
-  inverse <- NULL
+  # Assign a NULL value to the inverse
+  i <- NULL
+  # Function to set the matrix
   set <- function(y){
     x <<- y
-    inverse <<- NULL
+    i <-- NULL
   }
-  get <- function() x
-  setinverse <- function(solveMatrix) inverse <<- solveMatrix
-  getinverse <- function() inverse
+  # Function to get the matrix
+  get <- function(){
+    x
+  }
+  # Function to set the inverse of the matrix
+  setinverse <- function(inverse){
+    i <<- inverse
+  }
+  # Function to get the inverse of the matrix
+  getinverse <- function(){
+    i
+  }
+  # Return a list of the functions
   list(set = set, get = get, setinverse = setinverse, getinverse = getinverse)
 }
 
@@ -22,13 +34,29 @@ makeCacheMatrix <- function(x = matrix()) {
 # cacheSolve: This function computes the inverse of the special "matrix" returned by makeCacheMatrix above
 
 cacheSolve <- function(x, ...) {
-  inverse <- x$getinverse()
-  if(!is.null(inverse)){
-    message("Cached Data")
-    return(inverse)
+  # Return a matrix that is the inverse of 'x'
+  i <- x$getinverse()
+  # Just return the inverse if it's already cached
+  if (!is.null(i)){
+    message("Inverse Cached Data")
+    return(i)
   }
+  # Get matrix from our object
   data <- x$get()
-  inverse <- solve(data)
-  x$setinverse(inverse)
-  inverse      
+  # Calculate the inverse
+  i <- solve(data, ...)
+  # Set inverse in the object
+  x$setinverse(i)
+  i  
 }
+
+# Question 3
+# Example
+
+example <- makeCacheMatrix(matrix(c(3, 2, 5, 2, 3, 2, 5, 2, 4), nrow = 3, ncol = 3)) # Cache matrix
+example$get() # Retrieve the matrix
+cacheSolve(example) # Calculate the inverse of the matrix
+cacheSolve(example) # Retrieve the inverse now that it has been cached
+example$getinverse() # Verify that the inverse was cached
+
+# To prove another example, just write example$set("desired matrix")
